9e95192c3227d04bbaac610c23b0dafab52c4158
bgops <bgops@test1.klin-pro.com>
2018-06-13 23:34:19 -0700

    bla

--- klinenv.go
+++ klinenv.go
@@ -1,56 +1,57 @@
 package klinenv
 
 import (
-    "bufio"
-    "fmt"
-    "log"
-    "os"
-    "strings"
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+	"strings"
 )
 
 type Config interface {
-    Get(string) (string, error)
+	Get(string) (string, error)
 }
 
 type AppConfig struct {
-    data map[string]string
+	data map[string]string
 }
 
 func (ac AppConfig) Get(key string) (string, error) {
-    value, ok := ac.data[key]
-    if !ok {
-        return "", fmt.Errorf("attempt to read nonexistent config value %s", key)
-    }
-    return value, nil
+	value, ok := ac.data[key]
+	if !ok {
+		return "", fmt.Errorf("attempt to read nonexistent config value %s", key)
+	}
+	return value, nil
 }
 func NewAppConfig(filename string) AppConfig {
-    fin, err := os.Open(filename)
-    if err != nil {
-        log.Fatal(err)
-    }
-    defer func() {
-        cerr := fin.Close()
-        if cerr != nil {
-            log.Fatal(cerr)
-        }
-    }()
-    scanner := bufio.NewScanner(fin)
-    scanner.Split(bufio.ScanLines)
-    config := AppConfig{}
-    config.data = make(map[string]string, 0)
-    for scanner.Scan() {
-        line := scanner.Text()
-        chunks := strings.Split(line, "=")
-        if len(chunks) != 2 {
-            final := line[len(chunks[0])+1:]
-            config.data[chunks[0]] = strings.Trim(final,"\"")
-            continue
-            log.Fatal("config error at ",chunks[0])
-        } else {
-            config.data[chunks[0]] = strings.Trim(chunks[1],"\"")
-        }
-    }
-    return config
+	fin, err := os.Open(filename)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer func() {
+		cerr := fin.Close()
+		if cerr != nil {
+			log.Fatal(cerr)
+		}
+	}()
+	scanner := bufio.NewScanner(fin)
+	scanner.Split(bufio.ScanLines)
+	config := AppConfig{}
+	config.data = make(map[string]string, 0)
+	for scanner.Scan() {
+		line := scanner.Text()
+		if line == "\n" {
+			continue
+		}
+		chunks := strings.Split(line, "=")
+		if len(chunks) != 2 {
+			final := line[len(chunks[0])+1:]
+			config.data[chunks[0]] = strings.Trim(final, "\"")
+			continue
+			log.Fatal("config error at ", chunks[0])
+		} else {
+			config.data[chunks[0]] = strings.Trim(chunks[1], "\"")
+		}
+	}
+	return config
 }
-
-
